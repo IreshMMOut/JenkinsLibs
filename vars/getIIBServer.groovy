1a688a6cbc7d5c6f30bfe7931ed46b0c94777885
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-05-28 13:40:01 +0530

    purpose: testing

--- vars/getIIBServer.groovy
+++ vars/getIIBServer.groovy
@@ -1,3 +1,4 @@
 def call(def branchName) {
-    ["-i ace1 -p 4414"] 
+    ["-i ${getActiveACENode('10.243.64.6', '10.243.64.7', '4414')} -p 4414",
+    "-i ${getActiveACENode('10.243.64.7', '10.243.64.6', '4415')} -p 4415"]
 }
